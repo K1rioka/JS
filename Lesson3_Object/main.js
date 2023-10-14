71eb71b7799e79c34559b56597c5c00ba4a2037f
K1rioka <eshopokov@gmail.com>
2023-10-14 21:14:47 +0630

    smth

---
+++ Lesson3_Object/main.js
@@ -0,0 +1,34 @@
+let a = 5
+
+let b = a
+
+a=10
+
+console.log(b);
+
+//!Object
+
+// const obj = {
+//     name : 'Emir',
+//     age : 21
+// }
+
+// const obj2 = obj
+
+// obj.age = 30
+
+// console.log(obj2);
+
+//? Присвоение 
+
+const obj = {
+    name: 'smth',
+    age : 20
+}
+
+obj.name = 'Gaysal'
+obj['age'] = 1000
+obj.language = 'Hilichurlish'
+obj.country = 'Bing Chilling'
+
+console.log(obj);
---
+++ Lesson_test/script.js
@@ -0,0 +1,26 @@
+// console.log(5);
+// console.error(4);
+// console.warn(3);
+// console.group('text', [2, 3, 4, 5]);
+
+// ! Типы данных - всего их 8
+
+//? number - числовой
+//? string - строковый
+//? boolean - 1/0, true/false, истинна/ложь - булевый
+//? null - пустота
+//? undefined - неизвестный
+//? bigint -большое число
+//? symbol - символ
+//? object - обьект
+
+//! Variables - Переменные
+
+var a = 5;
+let b = 5;
+const c = 5;
+
+b=10;
+console.log(b)
+
+// Создание всех типов переменных
